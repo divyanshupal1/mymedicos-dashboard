d1ecc88666ddde2df0efc6327ff2f02ee8d2662f
divyanshupal1 <divyanshupal77@gmail.com>
2023-11-18 02:28:47 +0530

    logout fix

--- app/api/auth/logout/route.js
+++ app/api/auth/logout/route.js
@@ -1,18 +1,10 @@
-import { NextResponse } from "next/server";
-
-
-export async function GET() {
-    try {
-        const response = NextResponse.json(
-            {
-                message: "Logout successful",
-                success: true,
-            }
-        )
-        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0)});
-        return response;
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
-        
-    }
+import { cookies } from 'next/headers'
+ 
+export async function GET(request) {
+  const cookieStore = cookies()
+  const token = cookieStore.set('token', '', { expires: new Date(0) })
+ 
+  return new Response(JSON.stringify({message: "Logout successful",success: true,}), {
+    status: 200,
+  })
+}
