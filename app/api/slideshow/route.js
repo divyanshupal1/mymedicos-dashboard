9404556784fd13f66ed7a547bd9a2c147aab0869
divyanshupal1 <divyanshupal77@gmail.com>
2023-11-24 12:50:36 +0530

    slideshow api

---
+++ app/api/slideshow/route.js
@@ -0,0 +1,11 @@
+import { db} from "@/lib/firebase"
+import { doc,getDoc,getDocs,collection} from "firebase/firestore"; 
+export const dynamic = 'force-dynamic'
+export async function GET(Request){   
+        var temp = []
+        const querySnapshot = await getDocs(collection(db, "Slideshow"));
+        querySnapshot.forEach((doc) => {
+             temp = [...temp,doc.data()]
+        });  
+        return new Response(JSON.stringify({status:"success",data:temp}))
+}
