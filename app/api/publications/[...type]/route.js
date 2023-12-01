d74bcabbaccb99519ec86058c6fca49ed98d45ae
divyanshupal1 <divyanshupal77@gmail.com>
2023-12-01 13:54:01 +0530

    pub fixes

--- app/api/publications/[...type]/route.js
+++ app/api/publications/[...type]/route.js
@@ -8,7 +8,18 @@ export async function GET(req,{params}) {
     var type = params.type;
     var data = undefined;
     var speciality = urlMerger(params.type[0])
-    if(type.length == 1){
+    if(speciality == "all"){
+        await admin.firestore().collection("Publications").get()
+        .then((querySnapshot) => {
+            data = [];
+            querySnapshot.forEach((doc) => {
+                var docData = doc.data();
+                var temp = {id:doc.id,data:{...docData,URL:""}};
+                data.push(temp);
+            });
+        })
+    }
+    else if(type.length == 1 && speciality != "all"){
         await admin.firestore().collection("Publications").where('Subject','==',speciality).get()
         .then((querySnapshot) => {
             data = [];
