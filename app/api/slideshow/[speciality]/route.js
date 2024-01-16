d9d0ccb18c542c7333a4ae973c4d9cdeb704f43b
divyanshupal1 <divyanshupal77@gmail.com>
2024-01-16 14:48:05 +0530

    slideshow route fix

--- app/api/slideshow/[speciality]/route.js
+++ app/api/slideshow/[speciality]/route.js
@@ -4,13 +4,13 @@ import {getDocs,collection} from "firebase/firestore";
 import admin from "@/lib/firebase-admin";
 export const dynamic = 'force-dynamic'
 export async function GET(Request,{params}){   
-        // var speciality = params.speciality
-        var speciality = 'Anatomy'
+        var speciality = params.speciality
+        // var speciality = 'Anatomy'
         speciality = urlMerger(speciality)
         var temp = []
-        // const querySnapshot = await getDocs(collection(db, "SlideShow",speciality));
-        const querySnapshot = await admin.firestore().collection("SlideShow",speciality).get();
-        console.log(querySnapshot.docs.length)
+        const querySnapshot = await getDocs(collection(db, "SlideShow",'Speciality',speciality));
+        // const querySnapshot = await admin.firestore().collection("SlideShow","Speciality",speciality).get();
+        // console.log(querySnapshot.docs.length)
         querySnapshot.forEach((doc) => {
              temp = [...temp,doc.data()]
         });  
